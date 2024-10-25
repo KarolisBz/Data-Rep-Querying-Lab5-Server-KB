7d7320db5e1abe83043b119005d5d643fd65f9d7
Karolis Bandziulis <g00417529@atu.ie>
2024-10-25 13:53:54 +0000

    Ex5: Complete

--- server.js
+++ server.js
@@ -21,6 +21,36 @@ app.get('/hello/:name/:surname', (req, res) => {
     res.send(`Hello ${name} ${surname}`);
 });
 
+// route parameter to fetch movies
+// with status code
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    // status code 200  means everything is okay
+    res.status(200).json({ myMovies:movies }); // name-value array
+});
+
 // severs listens for a http request coming in
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
