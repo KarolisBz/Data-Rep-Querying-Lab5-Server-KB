e6e0b5e4258a94f3708a7318d0c414b4de91629f
Karolis Bandziulis <g00417529@atu.ie>
2024-10-25 13:39:15 +0000

    EX 4: complete (route parameters)

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+// error handling middlware allows us to catch server errors
+// at the top because is is an interpreted language
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 // if we get a request, 'Welcome to Data Respresentation & Querying'
 app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
 });
 
-// error handling middlware allows us to catch server errors
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
+// route parameter
+app.get('/hello/:name/:surname', (req, res) => {
+    const name = req.params.name;
+    const surname = req.params.surname;
+    res.send(`Hello ${name} ${surname}`);
 });
 
 // severs listens for a http request coming in
